c40d726e189195496766d1dfe96a591ab38ba351
Rami <rmnaser90@gmail.com>
2022-01-03 17:28:04 +0200

    start working on canvas

---
+++ src/Componenets/Screens/DrawingScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Canvas from "../SubComponents/Canvas";
+
+const DrawingScreen = () => {
+  return (
+    <div>
+      <h1>drawing screen</h1>
+      <Canvas />
+    </div>
+  );
+};
+
+export default DrawingScreen;
